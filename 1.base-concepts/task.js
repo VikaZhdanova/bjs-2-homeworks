bed350886d507f95fc872570bbf94a9a529a8d0b
Vika Zhdanova <vikazhdanova97@mail.ru>
2021-09-06 13:13:09 +0300

    my first commit for hm1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,20 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  console.log(d);
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    console.log(x);
+    return [x];
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    console.log(x1);
+    console.log(x2);
+    return [x1,x2];
+  }
 }
