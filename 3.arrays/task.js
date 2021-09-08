df8918a5a5432e2e545b0999122697529fcc4ed6
Vika Zhdanova <vikazhdanova97@mail.ru>
2021-09-08 20:08:30 +0300

    commit for 3hw

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
+
+//Задача1
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = ((arr1.lenght === arr2.lenght) && (arr1.every((item, index) => item === arr2[index])));
   return result; // boolean
 }
 
+
+//Задача2
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
   return resultArr; // array
 }
